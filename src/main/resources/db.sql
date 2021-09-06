b7daac13e132c24ae11dfc170c4cfe51abe5fc40
alex <zhivoderov.aleksej@gmail.com>
2021-09-06 10:23:21 +0300

    Добавил запуск с командной строки и иницилазицию БД

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,17 +1,17 @@
---
--- create table orders(
---     id serial primary key,
---     order_number int,
---     consumer_email varchar(50),
---     create_date date
--- );
--- create table product(
---     price decimal,
---     delete boolean default false,
---     article int not null,
---     title varchar(50),
---     id serial primary key
--- );
+create database market;
+create table orders(
+    id serial primary key,
+    order_number int,
+    consumer_email varchar(50),
+    create_date date
+);
+create table product(
+    price decimal,
+    delete boolean default false,
+    article int not null,
+    title varchar(50),
+    id serial primary key
+);
 create table product_orders(
     order_id int not null,
     product_id int not null,
